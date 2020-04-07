6af0844e80405753d920e1dcabd3aa50832d861c
Ubuntu <ubuntu@ip-172-31-61-198.ec2.internal>
2020-04-07 18:28:22 +0000

    Added visualizations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import copy
+import pickle as pkl
 import numpy as np
 from tqdm.auto import tqdm
 import torch
@@ -10,6 +11,9 @@ from torch.utils.data import DataLoader
 import torchvision
 import torchvision.transforms as transforms
 import torch.nn.utils.prune as prune
+from tensorboardX import SummaryWriter 
+
+# from torch.utils.tensorboard import SummaryWriter
 
 
 #Custom libs
@@ -26,10 +30,10 @@ def main(args):
     #Hyper-parameters
     #TODO: interate the args
     num_prunes = args.n_prune
-    prune_amount = 0.1
+    prune_amount = args.prune_amount
     epochs = args.n_epoch
-    learning_rate = 1e-3
-    weightDecay = 1e-4
+    learning_rate = args.lr
+    weightDecay = args.weight_decay
     warm_up_k = args.n_warm_up
     warm_up_iter = 0
 
@@ -48,8 +52,11 @@ def main(args):
     training_dataset = datasets.MNIST(root='./data', transform=torchvision.transforms.Compose(train_transforms), download=True)
     validation_dataset = datasets.MNIST(root='./data', train= False, transform=torchvision.transforms.Compose(validation_transforms), download=True)
 
-    training_loader_args = dict(shuffle=True, batch_size=batch_size, num_workers=num_workers, pin_memory=True)\
-    if cuda else dict(shuffle=True, batch_size=batch_size)
+    training_loader_args = dict(shuffle=True, 
+                                batch_size=batch_size, 
+                                num_workers=num_workers, 
+                                pin_memory=True) if cuda else dict(shuffle=True, 
+                                                                    batch_size=batch_size)
     training_loader = DataLoader(training_dataset, **training_loader_args)
 
     validation_loader_args = dict(shuffle=False, batch_size=batch_size, num_workers=num_workers, pin_memory=True)\
@@ -69,24 +76,56 @@ def main(args):
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.5, patience=2)
     net.to(device)
 
+    writer = SummaryWriter()
+    
+    global_sparsity = 100
+    results = {}
+    results['train_accuracy'] = {}
+    results['test_accuracy'] = {}
+    results['train_loss'] = {}
+    results['test_loss'] = {}
+
     for prune_cycle in range(num_prunes):
-        for i in range(epochs):
-            print('Epoch: '+ str(i)+' ('+str(prune_cycle)+' prunings)')
+        results['train_accuracy']['prune_{0}'.format(global_sparsity)] = []
+        results['test_accuracy']['prune_{0}'.format(global_sparsity)] = []
+        results['train_loss']['prune_{0}'.format(global_sparsity)] = []
+        results['test_loss']['prune_{0}'.format(global_sparsity)] = []
+        for epoch in range(epochs):
+            print('Epoch: '+ str(epoch)+' ('+str(prune_cycle)+' prunings)')
 
             train_loss, train_acc, warm_up_iter = train_epoch(net, device, training_loader, criterion, optimizer, warm_up_iter, warm_up_k, learning_rate)
-            validation_loss, validation_acc = validatate_epoch(net, device, validation_loader, criterion, scheduler)
+            validation_loss, validation_acc = validate_epoch(net, device, validation_loader, criterion, scheduler)
 
+            writer.add_scalar('train/accuracy/prune_{0}'.format(global_sparsity), train_acc, epoch)
             print('Training Accuracy: ', train_acc, "%")
+            results['train_accuracy']['prune_{0}'.format(global_sparsity)].append(train_acc)
+
+            writer.add_scalar('train/loss/prune_{0}'.format(global_sparsity), train_loss, epoch)
             print('Training Loss: ', train_loss)
+            results['train_loss']['prune_{0}'.format(global_sparsity)].append(train_loss)
+            
+            writer.add_scalar('validation/accuracy/prune_{0}'.format(global_sparsity), validation_acc, epoch)
             print('Validation Accuracy: ', validation_acc, "%")
+            results['test_accuracy']['prune_{0}'.format(global_sparsity)].append(validation_acc)
+            
+            writer.add_scalar('validation/loss/prune_{0}'.format(global_sparsity), validation_loss, epoch)
             print('Validataion Loss: ', validation_loss)
+            results['test_loss']['prune_{0}'.format(global_sparsity)].append(validation_loss)
+
             print('='*50)
+
+        pkl.dump(results, open("results_prune_{0}.p".format(global_sparsity), "wb" ))
+            
 
         #TODO: implement a global pruning function
         #order of these two lines depends on how we implement prune
         #TODO: implement loading initial weights
         #net.load_state_dict(initial_state)
-        net = my_prune(net, prune_amount,initial_state)
+        net, global_sparsity = my_prune(net, prune_amount,initial_state)
+    
+    pkl.dump(results, open("results_final.p", "wb"))
+    writer.close()
+
 
 
 def train_epoch(model, device, train_loader, criterion, optimizer, k, warm_up, lr):
@@ -127,7 +166,7 @@ def train_epoch(model, device, train_loader, criterion, optimizer, k, warm_up, l
     training_acc = (correct_predictions_training/total_predictions_training)*100.0
     return (np.mean(batch_losses_training), training_acc, k)
 
-def validatate_epoch(model, device, validation_loader, criterion, scheduler):
+def validate_epoch(model, device, validation_loader, criterion, scheduler):
     with torch.no_grad():
         batch_losses_validation = []
 
@@ -166,7 +205,7 @@ def learning_rate_scheduler(my_optim, k, warm_up, lr):
         param_group['lr'] = (k / warm_up) * lr
     return k + 1
 
-def my_prune(net, prune_amount,initial_state):
+def my_prune(net,prune_amount,initial_state):
 
     #TODO: separate prune into custom lib
     #TODO: function naming 
@@ -203,9 +242,9 @@ def my_prune(net, prune_amount,initial_state):
     #global prune
     #TODO: change pruning method to L2-norm
     prune.global_unstructured(
-    parameters_to_prune,
-    pruning_method=prune.L1Unstructured,
-    amount=prune_amount,
+        parameters_to_prune,
+        pruning_method=prune.L1Unstructured,
+        amount=prune_amount,
     )
 
     #print global sparsity after pruning
@@ -263,16 +302,11 @@ def my_prune(net, prune_amount,initial_state):
     #load the initial weight
     for name, param in initial_state.items():
         if name not in net.state_dict():          
-            net.state_dict()[name+ '_orig'].copy_(param)
-            
+            net.state_dict()[name+ '_orig'].copy_(param) 
         else:
             net.state_dict()[name].copy_(param)
 
-
-
-    print(
-    "Global sparsity after loading: {:.2f}%".format(
-        100. * float(
+    global_sparsity = 100. * float(
             torch.sum(net.conv1.weight == 0)
             + torch.sum(net._modules['layer1'][0]._modules['conv1'].weight == 0)
             + torch.sum(net._modules['layer1'][0]._modules['conv2'].weight == 0)
@@ -294,8 +328,7 @@ def my_prune(net, prune_amount,initial_state):
             + torch.sum(net._modules['layer4'][1]._modules['conv1'].weight == 0)
             + torch.sum(net._modules['layer4'][1]._modules['conv2'].weight == 0)
             + torch.sum(net.fc.weight == 0)
-        )
-        / float(
+        ) / float(
             net.conv1.weight.nelement()
             + net._modules['layer1'][0]._modules['conv1'].weight.nelement()
             + net._modules['layer1'][0]._modules['conv2'].weight.nelement()
@@ -318,17 +351,19 @@ def my_prune(net, prune_amount,initial_state):
             + net._modules['layer4'][1]._modules['conv2'].weight.nelement()
              + net.fc.weight.nelement()
             )
-        )
-    )
-    return net
+    print( "Global sparsity after loading: {:.2f}%".format(global_sparsity))
+    return net, global_sparsity 
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     #added argument for parser
     parser.add_argument('--model', default='resnet18', help='')
-    parser.add_argument('--n_epoch', type=int, default=1,help='')
+    parser.add_argument('--n_epoch', type=int, default=10,help='')
     parser.add_argument('--n_prune', type=int, default=5,help='')
     parser.add_argument('--n_warm_up', type=int, default=20000,help='')
+    parser.add_argument('--prune_amount', type=float, default=0.1, help='')
+    parser.add_argument('--lr', type=float, default=1e-3, help='learning_rate')
+    parser.add_argument('--weight_decay', type=float, default=1e-4)
 
     args = parser.parse_args()
     main(args)
